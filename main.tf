747219f99bb75b57d349170964200bb8e29c52dc
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-19 16:19:43 +0530

    changes

--- main.tf
+++ main.tf
@@ -4,3 +4,25 @@ resource "aws_db_subnet_group" "default" {
 
   tags = merge(local.tags, { Name = "${local.name_prefix}-subnet" })
 }
+
+resource "aws_security_group" "main" {
+  name   = "${local.name_prefix}-sg"
+  vpc_id = var.vpc_id
+  tags   = merge(local.tags, { Name = "${local.name_prefix}-sg" })
+
+  ingress {
+    description = "RDS"
+    from_port   = var.sg_port
+    to_port     = var.sg_port
+    protocol    = "tcp"
+    cidr_blocks = var.ssh_ingress
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
--- vars.tf
+++ vars.tf
@@ -1,3 +1,6 @@
 variable "subnet_ids" {}
 variable "tags" {}
 variable "env" {}
+variable "vpc_id" {}
+variable "sg_port" {}
+variable "ssh_ingress" {}
