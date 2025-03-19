f8c286d161f56ffd79a275ec9d640e1964a6ce86
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-19 18:11:10 +0530

    changes

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "aws_rds_cluster" "main" {
   db_subnet_group_name            = aws_db_subnet_group.main.name
   vpc_security_group_ids          = [aws_security_group.main.id]
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.main.name
-  database_name                   = data.aws_ssm_parameter.database_name.name
-  master_username                 = data.aws_ssm_parameter.master_username.name
-  master_password                 = data.aws_ssm_parameter.master_password.name
+  database_name                   = data.aws_ssm_parameter.database_name.value
+  master_username                 = data.aws_ssm_parameter.master_username.value
+  master_password                 = data.aws_ssm_parameter.master_password.value
   backup_retention_period         = var.backup_retention_period
   preferred_backup_window         = var.preferred_backup_window
   skip_final_snapshot             = var.skip_final_snapshot
